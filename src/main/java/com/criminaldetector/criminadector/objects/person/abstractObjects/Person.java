90431dd0ea414e50b0f1dd676472fee244647ffe
Rene Ceasar Obeja <rcobeja24@gmail.com>
2022-07-12 16:35:41 +0800

    Added Person Object

---
+++ src/main/java/com/criminaldetector/criminadector/objects/person/abstractObjects/Person.java
@@ -0,0 +1,30 @@
+package com.criminaldetector.criminadector.objects.person.abstractObjects;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import lombok.Data;
+
+@MappedSuperclass
+public @Data class Person {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String name;
+
+    @Column(nullable = false, length = 255)
+    private String sex;
+
+    @Column(nullable = false)
+    private Long age;
+
+    @Column(nullable = false, length = 255)
+    private String role;
+
+}
