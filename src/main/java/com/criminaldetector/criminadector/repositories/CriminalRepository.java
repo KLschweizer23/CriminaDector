4309ee9b881330878afd2e2de971e30329a92dc8
Rene Ceasar Obeja <rcobeja24@gmail.com>
2022-07-12 17:03:51 +0800

    added Repository for Criminal

---
+++ src/main/java/com/criminaldetector/criminadector/repositories/CriminalRepository.java
@@ -0,0 +1,9 @@
+package com.criminaldetector.criminadector.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.criminaldetector.criminadector.objects.person.Criminal;
+
+public interface CriminalRepository extends JpaRepository<Criminal, Long>{
+    
+}
