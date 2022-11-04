1dfd107c101c40937471497aa2e753bc9d571af8
Ken Bill <schweizer23.ss@gmail.com>
2022-11-04 10:34:59 +0800

    added profile dynamicity

--- src/main/java/com/criminaldetector/criminadector/AppController.java
+++ src/main/java/com/criminaldetector/criminadector/AppController.java
@@ -1,13 +1,20 @@
 package com.criminaldetector.criminadector;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.criminaldetector.criminadector.repositories.CriminalRepository;
+import com.criminaldetector.criminadector.objects.person.Criminal;
+
 @RestController
 public class AppController {
     
+    @Autowired
+    CriminalRepository criminalRepository;
+
     @GetMapping("")
     public ModelAndView homePage(Model model){
         ModelAndView mv = new ModelAndView();
@@ -28,12 +35,17 @@ public class AppController {
         return mv;
     }
 
-    @GetMapping("/profile")
+    @GetMapping("/profile/{id}")
     public ModelAndView profilePage(Model model){
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("profile.html");
+        mv.setViewName("profile.html"); 
         
+        Criminal currentCriminal = criminalRepository.findById(1L).get();
+
+        System.out.println(currentCriminal.getName());
+
         model.addAttribute("title", "CriminaDector | Profile");
+        model.addAttribute("criminal", currentCriminal);
 
         return mv;
     }
