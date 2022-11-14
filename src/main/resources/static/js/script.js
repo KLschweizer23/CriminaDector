04641538299f2cf34158973f70d4c327a9a7dbf7
Ken Bill <schweizer23.ss@gmail.com>
2022-11-14 17:12:34 +0800

    face recognizer is integrated

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,42 +1,54 @@
+
+window.onerror = function() {
+    location.reload();
+}
 const video = document.getElementById('videoInput')
+const button = document.getElementById('videoStarter')
 
 Promise.all([
     faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
     faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
     faceapi.nets.ssdMobilenetv1.loadFromUri('./models') //heavier/accurate version of tiny face detector
-]).then(start)
+]).then(start);
 
 function start() {
-    document.body.append('Models Loaded')
-    
-    navigator.getUserMedia(
-        { video:{} },
-        stream => video.srcObject = stream,
-        err => console.error(err)
-    )
-    
-    //video.src = '../videos/speech.mp4'
-    console.log('video added')
-    recognizeFaces()
-}
+    var currentLocation = window.location.href;
+    if(findWord("dashboard", currentLocation)){
 
+        document.body.append('Models Loaded')
+        
+        navigator.getUserMedia(
+            { video:{} },
+            stream => video.srcObject = stream,
+            err => console.error(err)
+        )
+            
+            //video.src = '../videos/speech.mp4'
+            console.log('video added')
+            recognizeFaces()
+    }
+}
+function findWord(word, str) {
+    return str.split('/').some(function(w){return w === word})
+}
 async function recognizeFaces() {
 
     const labeledDescriptors = await loadLabeledImages()
     console.log(labeledDescriptors)
-    const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.7)
+    const faceMatcher = new faceapi.faceMatcher(labeledDescriptors, 0.7)
 
 
-    video.addEventListener('play', async () => {
+    //pag ma play
+    button.addEventListener('click', async() => {
         console.log('Playing')
         const canvas = faceapi.createCanvasFromMedia(video)
-        document.body.append(canvas)
+        document.getElementById('container').append(canvas)
 
         const displaySize = { width: video.width, height: video.height }
         faceapi.matchDimensions(canvas, displaySize)
 
         
-
+        //Pang square
         setInterval(async () => {
             const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors()
 
@@ -53,13 +65,12 @@ async function recognizeFaces() {
                 drawBox.draw(canvas)
             })
         }, 100)
-
-
-        
     })
+    // video.addEventListener('play', async () => {
+    // })
 }
 
-
+//load images
 function loadLabeledImages() {
     //const labels = ['Black Widow', 'Captain America', 'Hawkeye' , 'Jim Rhodes', 'Tony Stark', 'Thor', 'Captain Marvel']
     const labels = ['KL'] // for WebCam
